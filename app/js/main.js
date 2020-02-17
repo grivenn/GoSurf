269607e646a313a0c47ed3a31c30242c5cb90abc
Олег <E:\Distr\'Backup\TheBat>
2020-02-17 06:49:41 +0300

    dev

--- app/js/main.js
+++ app/js/main.js
@@ -5,11 +5,11 @@ $(function () {
         fade: true,
         prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt="" />',
         nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt="" />',
-        asNavFor: '.slider-dots'
+        asNavFor: '.slider-dotshead'
 
     });
 
-    $('.slider-dots').slick({
+    $('.slider-dotshead').slick({
         slidesToShow: 4,
         slidesToScroll: 4,
         asNavFor: '.header__slider',
